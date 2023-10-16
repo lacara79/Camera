8951debb752d54e918eac32bd80f2f7b3884fdd8
Luiz Alexandre da Costa Araujo <48302746+lacara79@users.noreply.github.com>
2023-10-16 14:08:38 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
   constructor() {}
+  photo: any = "";
 
+  constructor() {}
+  
+  async abrir(){
+    const takePicture = async () => {
+      const image = await Camera.getPhoto({
+        quality: 100,
+        allowEditing: true,
+        resultType: CameraResultType.DataUrl,
+        saveToGallery: true
+      });
+    
+      // const theActualPicture = image.dataUrl;
+      this.photo = image.dataUrl;
+    };
+    takePicture();
+  }
 }
